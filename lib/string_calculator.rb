7267086a5a49c7c8b0b6743bac470f4d9ff95152
Jesal Rathod <rathodjesal7@gmail.com>
2025-05-27 12:41:17 +0530

    refactor: extract input type, delimiters parsing and negative values logic into a separate methods to keep add method clean and improve readability

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,25 +1,41 @@
 class StringCalculator
   def add(numbers)
-    raise ArgumentError, "Input must be a string" unless numbers.is_a?(String)
+    validate_input_type(numbers)
+
     return 0 if numbers.empty?
-    parsed_numbers = begin
-                       numbers.gsub(/[^0-9\-,]/, ',')
-                              .split(',')
-                              .map(&:strip)
-                              .reject(&:empty?)
-                              .map(&:to_i)
-                     rescue
-                       raise ArgumentError, "Invalid input format"
-                     end
 
-    raise ArgumentError, "No valid numbers found" if parsed_numbers.empty?
+    parsed_numbers = parse_numbers(numbers)
+    validate_parsed_numbers(parsed_numbers)
+    check_for_negatives(parsed_numbers)
 
-    negatives = parsed_numbers.select { |n| n < 0 }
+    parsed_numbers.sum
+  end
+
+  private
+
+  def validate_input_type(numbers)
+    raise ArgumentError, "Input must be a string" unless numbers.is_a?(String)
+  end
+
+  def parse_numbers(numbers)
+    numbers.gsub(/[^0-9\-,]/, ',')
+           .split(',')
+           .map(&:strip)
+           .reject(&:empty?)
+           .map(&:to_i)
+  rescue
+    raise ArgumentError, "Invalid input format"
+  end
+
+  def validate_parsed_numbers(numbers)
+    raise ArgumentError, "No valid numbers found" if numbers.empty?
+  end
+
+  def check_for_negatives(numbers)
+    negatives = numbers.select { |n| n < 0 }
     unless negatives.empty?
       raise ArgumentError, "negative numbers not allowed: #{negatives.join(', ')}"
     end
-
-    parsed_numbers.sum
   end
 end
 
